7d7ec5a8a12049df31de2034952f3b72c1f53e43
Elisee-des <esabidani@gmail.com>
2022-07-16 22:46:21 +0000

    Ok pour le routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/article', function () {
+    return response()->json([
+        'titre' => 'Mon titre',
+        'description' => 'Ma description'
+    ]);
+});
+
+Route::get('/articles', function () {
+    return view('articles');
+});
